8938486b53ddff0dd5ad80ff7574e1c8f834ea5c
student <bettyjcawthon@gmail.com>
2019-01-16 12:02:46 -0600

    switchcase morn cw

---
+++ Problem2.js
@@ -0,0 +1,41 @@
+//Put the start of your program in a main function. Ask the user to enter a 3 digit binary number. Create a switch case
+// that prints 1 if you enter 1, 2 if you enter 10, 3 if you enter 11, 4 if you enter 100, and 5 if you enter 101.
+
+main();
+  function main()
+  {
+      var expr = prompt("Enter 3 digit binary number");
+      switch (expr)
+      {
+          case "1":
+              console.log("1");
+              break;
+          case "10":
+              console.log("2");
+              break;
+          case "11":
+               console.log("3");
+               break;
+          case "100":
+               console.log("4");
+               break;
+          case "101":
+              console.log("5");
+              break;
+          default:
+              console.log("error");
+
+
+
+
+
+
+
+
+      }
+
+
+
+
+
+  }
---
+++ Problem3.js
@@ -0,0 +1,58 @@
+//Put the start of your program in a main function. Create an attendance array. Ask the user if a teacher, student, or
+// parent is checking in. Use a switch statement to add the teacher/student/parent to the attendance array, then log the
+// option they chose. Repeat this five times.
+//
+// Challenge: Do the above program until the user enter's 'q'. Also, print and add to the array via a function call to
+// the appropriate teacher/student/parent function.
+
+
+main();
+    function main()
+    {
+        attendance = [];
+        var attendance = ["teacher", "student", "parent"];
+
+
+
+
+
+        var attendance = prompt("Who is checking in?");
+        switch (attendance)
+        {
+            case "[attendance]":
+                console.log("");
+                break;
+            case "[attendance]":
+                console.log("");
+                break;
+            case "[attendance]":
+                console.log("");
+                break;
+            case "[attendance]":
+                console.log("");
+                break;
+            case "[attendance]":
+                console.log("");
+                break;
+            default:
+                console.log("error");
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
